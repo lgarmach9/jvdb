df6817340a1cac0377503a6812becc5240c81ef9
lgarmach9 <LGARCIAM814@alumnos.imf.com>
2024-01-14 12:37:22 +0100

    Clase funcionando

--- conexion.py
+++ conexion.py
@@ -8,7 +8,7 @@ class Jvdb:
         self.coleccion = coleccion
         self.documento = documento
         self.contenido = contenido
-        comando = '"C:\\Users\\laura\\Documents\\GitHub\\jvdb\\jvdb.exe" '+operacion+' '+basededatos+' '+coleccion+' '+documento+' '+contenido+''
+        comando = '"C:\\Users\\laura\\Documents\\GitHub\\jvdb\\jvdb.exe" '+self.operacion+' '+self.basededatos+' '+self.coleccion+' '+self.documento+' '+self.contenido+''
         resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
         if resultado.returncode == 0:
             print("ok")
@@ -16,3 +16,4 @@ class Jvdb:
             return("ko")
 
 Conexion1= Jvdb("miempresa")
+Conexion1.insert("clientes", "clientes7", "este es otro contenido de prueba")
