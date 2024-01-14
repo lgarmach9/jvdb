1558684dfb01099c5c88a300feafcc6ec07a6e8e
lgarmach9 <LGARCIAM814@alumnos.imf.com>
2024-01-14 12:33:33 +0100

    Objeto

--- conexion.py
+++ conexion.py
@@ -1,17 +1,18 @@
 import subprocess
 
-class jvdb:
-    def __init__(self,basededatos)
+class Jvdb:
+    def __init__(self,basededatos):
         self.basededatos = basededatos
     def insert(self,coleccion,documento,contenido):
         self.operacion = "insert"
         self.coleccion = coleccion
         self.documento = documento
         self.contenido = contenido
-            comando = '"C:\\Users\\laura\\Documents\\GitHub\\jvdb\\jvdb.exe" '+operacion+' '+basededatos+' '+coleccion+' '+documento+' '+contenido+''
-            resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
+        comando = '"C:\\Users\\laura\\Documents\\GitHub\\jvdb\\jvdb.exe" '+operacion+' '+basededatos+' '+coleccion+' '+documento+' '+contenido+''
+        resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
+        if resultado.returncode == 0:
+            print("ok")
+        else:
+            return("ko")
 
-            if resultado.returncode == 0:
-                print("ok")
-            else:
-                return("ko")
+Conexion1= Jvdb("miempresa")
