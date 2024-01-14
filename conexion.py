fb77d595a4d6bc470da03c6b28a5c522d033b3bc
lgarmach9 <LGARCIAM814@alumnos.imf.com>
2024-01-14 12:26:35 +0100

    Parametrización

--- conexion.py
+++ conexion.py
@@ -1,6 +1,13 @@
 import subprocess
 
-comando = "C:\\Users\\laura\\Documents\\GitHub\\jvdb\\jvdb.exe insert miempresa clientes cliente4 'este es el cliente 4'"
+operacion = "insert"
+basededatos = "miempresa"
+coleccion = "clientes"
+documento = "cliente5"
+contenido = "esto es otro contenido de prueba"
+
+
+comando = '"C:\\Users\\laura\\Documents\\GitHub\\jvdb\\jvdb.exe" '+operacion+' '+basededatos+' '+coleccion+' '+documento+' '+contenido+''
 resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
 
 if resultado.returncode == 0:
